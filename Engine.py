853bf46deef3682559260fa5d4bfae5fc2afa720
KulsoomPatel <kulsoom-patel@hotmail.co.uk>
2017-03-26 15:09:07 +0100

    All of logic which does need fixing

---
+++ Engine.py
@@ -0,0 +1,66 @@
+import pygame
+import sys
+
+from Food import Food
+
+
+class Engine:
+    def __init__(self):
+        pass
+
+    def release_batch(self, screen, settings, foods):
+        for x in range(0, settings.batch_size):
+            self.spawn_foods(screen, settings, foods)
+
+    def check_foods(self, foods, basket, scoreboard, screen, settings, time_passed):
+        for food in foods:
+            food.update(time_passed)
+            food.blitme()
+
+            if food.rect.colliderect(basket.rect):
+                self.catch_food(scoreboard, settings, food, foods)
+                continue
+
+            if food.y_position > screen.get_height():
+                self.miss_food(scoreboard, food, foods)
+                self.spawn_foods(screen, settings, foods)
+                continue
+
+            if scoreboard.food_missed > settings.misses_allowed:
+                # Set game_active to false, empty the list of balloons, and increment games_played
+                settings.game_active = False
+                settings.games_played += 1
+
+    def update_basket(self, basket, mouse_x):
+        basket.x_position = mouse_x
+        basket.update_rect()
+        basket.blitme()
+
+    def miss_food(self, scoreboard, food, foods):
+        scoreboard.food_missed += 1
+        foods.remove(food)
+
+    def catch_food(self, scoreboard, settings, food, foods):
+        scoreboard.food_caught += 1
+        foods.remove(food)
+        settings.food_speed *= settings.speed_increase_factor
+
+        if scoreboard.food_caught % settings.catch_needed == 0:
+            settings.batch_size += 1
+
+    def spawn_foods(self, screen, settings, foods):
+        foods.append(Food(screen, settings))
+
+    def check_events(self, settings, scoreboard, play_button, mouse_x, mouse_y, foods):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.rect.collidepoint(mouse_x, mouse_y):
+                    # Play button has been pressed.  Empty list of balloons,
+                    #  initialize scoreboard and game parameters, and make game active.
+                    del foods[:]
+                    # scoreboard.initialize_stats()
+                    settings.initialize_game_parameters()
+                    settings.game_active = True
--- food_fetcher.py
+++ food_fetcher.py
@@ -1,14 +1,14 @@
 import pygame
-import sys
-from Food import Food
 from Basket import Basket
 from Scoreboard import Scoreboard
 from Settings import Settings
 from Button import Button
+from Engine import Engine
 
 
 def run_game():
     settings = Settings()
+    engine = Engine()
 
     # initialise the game
     pygame.init()
@@ -31,17 +31,17 @@ def run_game():
         time_passed = clock.tick(50)
         mouse_x = pygame.mouse.get_pos()[0]
         mouse_y = pygame.mouse.get_pos()[1]
-        check_events(settings, scoreboard, play_button, mouse_x, mouse_y, foods)
+        engine.check_events(settings, scoreboard, play_button, mouse_x, mouse_y, foods)
 
         screen.fill(settings.bg_color)
 
         if settings.game_active:
-            update_basket(basket, mouse_x)
-            check_foods(foods, basket, scoreboard, screen, settings, time_passed)
+            engine.update_basket(basket, mouse_x)
+            engine.check_foods(foods, basket, scoreboard, screen, settings, time_passed)
 
             if len(foods) == 0:
                 settings.food_speed *= settings.speed_increase_factor
-                release_batch(screen, settings, foods)
+                engine.release_batch(screen, settings, foods)
         else:
             play_button.blitme()
             # If a game has just ended, show Game Over button
@@ -50,72 +50,7 @@ def run_game():
 
                 # Display scoreboard
         scoreboard.blitme()
-        play_button.blitme()
         pygame.display.flip()
-
-
-def spawn_foods(screen, settings, foods):
-    foods.append(Food(screen, settings))
-
-
-def miss_food(scoreboard, food, foods):
-    scoreboard.food_missed += 1
-    foods.remove(food)
-
-
-def catch_food(scoreboard, settings, food, foods):
-    scoreboard.food_caught += 1
-    foods.remove(food)
-    settings.food_speed *= settings.speed_increase_factor
-
-    if scoreboard.food_caught % settings.catch_needed == 0:
-        settings.batch_size += 1
-
-
-def update_basket(basket, mouse_x):
-    basket.x_position = mouse_x
-    basket.update_rect()
-    basket.blitme()
-
-
-def release_batch(screen, settings, foods):
-    for x in range(0, settings.batch_size):
-        spawn_foods(screen, settings, foods)
-
-
-def check_foods(foods, basket, scoreboard, screen, settings, time_passed):
-    for food in foods:
-        food.update(time_passed)
-        food.blitme()
-
-        if food.rect.colliderect(basket.rect):
-            catch_food(scoreboard, settings, food, foods)
-            continue
-
-        if food.y_position > screen.get_height():
-            miss_food(scoreboard, food, foods)
-            spawn_foods(screen, settings, foods)
-            continue
-
-        if scoreboard.food_missed > settings.misses_allowed:
-            # Set game_active to false, empty the list of balloons, and increment games_played
-            settings.game_active = False
-            settings.games_played += 1
-
-
-def check_events(settings, scoreboard, play_button, mouse_x, mouse_y, foods):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if play_button.rect.collidepoint(mouse_x, mouse_y):
-                # Play button has been pressed.  Empty list of balloons,
-                #  initialize scoreboard and game parameters, and make game active.
-                del foods[:]
-                scoreboard.initialize_stats()
-                settings.initialize_game_parameters()
-                settings.game_active = True
 
 
 run_game()
